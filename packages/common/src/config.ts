569648f0e9624b03b2d5d842af14954b886e5811
fewensa <37804932+fewensa@users.noreply.github.com>
2024-10-25 17:38:04 +0800

    Fix reporter by code review (#19)
    * fix bugs
    
    * try fetch api 3 times
    
    * fix bigint issue
    
    * udpate by suggestion

--- packages/common/src/config.ts
+++ packages/common/src/config.ts
@@ -2,7 +2,8 @@
 export interface XAPIConfigType {
   graphql: {
     endpoint: (chain: string) => string,
-  }
+  },
+  minimumRewards: Record<string, bigint>,
 };
 
 const graphqlEndpoint: Record<string, string> = {
@@ -13,11 +14,16 @@ const graphqlEndpoint: Record<string, string> = {
   // 'crab-dvm': 'https://thegraph.darwinia.network/dip7/subgraphs/name/crab',
 };
 
+const minimumRewards: Record<string, bigint> = {
+  sepolia: 12000000000000n, // $0.03
+};
+
 export const XAPIConfig: XAPIConfigType = {
   graphql: {
     endpoint(chain: string): string {
       return graphqlEndpoint[chain] ?? graphqlEndpoint._fallback;
     }
-  }
+  },
+  minimumRewards,
 };
 
