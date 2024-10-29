46f01b18e850d31ee96e474d3340d93fb67bfb12
Guantong <i@gt.email>
2024-10-29 15:45:03 +0800

    near indexer key

--- packages/common/src/config.ts
+++ packages/common/src/config.ts
@@ -8,7 +8,7 @@ export interface XAPIConfigType {
 
 const graphqlEndpoint: Record<string, string> = {
   _fallback: 'https://thegraph.darwinia.network/dip7/subgraphs/name/dip7index-crab',
-  near: 'https://api.studio.thegraph.com/query/66211/xapi-near/version/latest',
+  near: 'https://gateway.thegraph.com/api/330a1e1b847db8629cf21717988bbee7/subgraphs/id/8xQbVsestNMUK3vgpsGbJCqugfwpqS7GSbAcmWR21o6Y',
   sepolia: 'https://api.studio.thegraph.com/query/51152/txapi-sepolia/version/latest',
   // 'darwinia-dvm': 'https://thegraph.darwinia.network/dip7/subgraphs/name/darwinia',
   // 'crab-dvm': 'https://thegraph.darwinia.network/dip7/subgraphs/name/crab',
